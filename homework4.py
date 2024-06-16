a88a23c543fcb397b2bf198018ca38a4dcca2caf
ATarasov11 <kapitan-tarasov@mail.ru>
2024-06-16 13:38:09 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input("Введите Ваше отчество: ")
+print("В Вашем отчестве", len(my_string), "символов")
+my_string = "Действия с методами строк"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
